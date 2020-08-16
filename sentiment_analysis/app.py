8dde8a9d248d16820805bc2024ae2fbfb988f69c
Siphu Langeni, MS <szlangeni@gmail.com>
2020-08-16 15:59:56 -0400

    Add sentiment_prediction

--- sentiment_analysis/app.py
+++ sentiment_analysis/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from models import MaskSentimentClassifier
 
 app = FastAPI()
 
+@app.post('/classify_mask_sentiment')
+async def sentiment_prediction(tweet):
+    clf = MaskSentimentClassifier()
+    preprocessed_tweet = clf.preprocess(tweet)
+    prediction = clf.predict(preprocessed_tweet)
+    return {'tweet': tweet, 'label': prediction}
 
 @app.get('/healthcheck', status_code=200)
 async def healthcheck():
-    return 'the app is healthy'
+    return 'Covid mask sentiment analysis app is healthy'
