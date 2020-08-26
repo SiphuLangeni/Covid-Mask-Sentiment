cab53833daa99bf306a65d09e94d2bd4994f9772
Mandy Gu <32079152+happilyeverafter95@users.noreply.github.com>
2020-08-26 18:06:19 -0400

    Update app.py

--- sentiment_analysis/app.py
+++ sentiment_analysis/app.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
-from sentiment_analysis.models import MaskSentimentAnalysis
+
 
 app = FastAPI()
 
-@app.post('/classify_mask_sentiment')
-async def sentiment_prediction(tweet):
-    clf = MaskSentimentClassifier()
-    preprocessed_tweet = clf.preprocess(tweet)
-    prediction = clf.predict(preprocessed_tweet)
-    return {'tweet': tweet, 'label': prediction}
 
 @app.get('/healthcheck', status_code=200)
 async def healthcheck():
