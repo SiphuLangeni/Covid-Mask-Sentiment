3e140632aca4f853daec77d84ffa13f31641f548
happilyeverafter95 <mgu@wealthsimple.com>
2020-08-27 10:23:39 -0400

    migration

--- sentiment_analysis/db/models.py
+++ sentiment_analysis/db/models.py
@@ -7,13 +7,13 @@ Base = declarative_base()
 
 class Tweets(Base):
     __tablename__ = 'tweets'
-    tweet_id = Column(String(20), nullable=False, primary_key=True)
-    tweet_created_at = Column(DateTime(), nullable=False)
-    tweet = Column(String(2000), nullable=False)
-    hashtags = Column(ARRAY(String(500), zero_indexes=True), nullable=True)
-    label = Column(String(8), nullable=True)
-    annotated_at = Column(DateTime(), nullable=True)
-    updated_at = Column(DateTime(), nullable=False)
+    tweet_id = Column(String, nullable=False, primary_key=True)
+    tweet_created_at = Column(DateTime, nullable=False)
+    tweet = Column(String, nullable=False)
+    hashtags = Column(ARRAY(String, zero_indexes=True), nullable=True)
+    label = Column(String, nullable=True)
+    annotated_at = Column(DateTime, nullable=True)
+    updated_at = Column(DateTime, nullable=False)
 
     def __repr__(self):
         return(f'Tweet {self.tweet_id} created @ {self.tweet_created_at}:\n\n{self.tweet}')
